593d22a4268708d1a013a95bdc7cba3b800e08f3
chaurasiavipin <vipinchaurasia481@gmail.com>
2026-01-05 22:41:33 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,19 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [name, setAp]=useState('')
   return (
     <div>
         {/* Do not remove the main div */}
+    <p>Enter your name:</P
+    <input
+    type="text"
+    value={name}
+      onChange={(e)=>setAp(e.target.value)}
+      />
+      {
+       name ? <p>Hello {name}!</p> : null
+}
     </div>
   )
 }
